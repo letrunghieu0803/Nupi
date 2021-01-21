d67dd29ee3583fd5c14cd85bafee72b59d4f6b31
Lê Hiếu <letrunghieu08031998@gmail.com>
2021-01-21 19:23:04 +0700

    xoa import bootstrap

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import allReducers from "./reducers/index";
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 const store = createStore(
